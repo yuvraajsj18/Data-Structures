be12765d8a1d55018a81501766400dd3a0e129f6
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2020-08-17 19:28:34 +0530

    stack as array first implementation

--- workspace/stack/array-implementation/stack.cpp
+++ workspace/stack/array-implementation/stack.cpp
@@ -5,11 +5,13 @@
 #include <iostream>
 #include <stdexcept>
 
+using namespace std;
+
 template <typename type, int size>
 class Stack
 {
     private:
-        type* stack; // stack is a dynamic array that will contains all our elements.
+        type* stack; // stack is a dynamically allocated array that will contains all our elements.
         int top;    // index of top element in stack
 
     public:
@@ -21,12 +23,81 @@ class Stack
         type pop();     // remove element at the top of stack if not underflow
         void display();     // print all elements in stack to console 
         type get_top_element();     // returns the top element
-        void clear();       // remove all elements in the stack and reset it.
+        void clear();       // remove all elements in the stack and reset top.
 };
 
 int main()
 {
+    cout << "Stack implemented as Array";
 
+    const int stack_size = 5;
+    Stack<int, stack_size> my_stack;
+
+    int choice;
+    while(true)
+    {
+        cout << "\nChoose an operation:\n";
+        cout << "1. Push\n" << "2. Pop\n" << "3. Display\n"
+        << "4. Display top\n" << "5. Stack empty?\n" << "6. Clear stack\n" 
+        << "7. Exit" << endl;
+        cout << "Enter a choice(1-6): ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                int element;
+                cout << "Enter element to push: ";
+                cin >> element;
+                try
+                {
+                    my_stack.push(element);
+                    cout << "Pushed to stack\n";
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << e.what() << '\n';
+                }
+            break;
+            case 2:
+                try
+                {
+                    int popped_element = my_stack.pop();
+                    cout << "Top most element popped: " << popped_element << endl;                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << e.what() << '\n';
+                }
+            break;
+            case 3:
+                cout << "Stack right now:\n";
+                my_stack.display();
+            break;
+            case 4:
+                try
+                {
+                    cout << "Top most element: " << my_stack.get_top_element() << endl;
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << e.what() << '\n';
+                }
+            break;
+            case 5:
+                my_stack.is_empty() ? cout << "Stack is empty right now.\n" : cout << "Stack is not empty right now.\n";
+            break;
+            case 6:
+                my_stack.clear();
+                cout << "Stack cleared.\n";
+            break;
+            case 7:
+                exit(0);
+            break;
+            default:
+                cout << "Invalid Input!! Please Try Again.\n" << endl; 
+            break;
+        }
+    }
 
     return 0;
 }
@@ -37,7 +108,7 @@ template <typename type, int size>
 Stack<type, size> :: Stack()
 {
     // create a stack with array of size - 'size'
-    stack = new T[size];
+    stack = new type[size];
     top = -1;   // -1 indicates that there is not element in stack at this moment
 }
 
@@ -77,7 +148,7 @@ type Stack<type, size> :: pop()
     // check for underflow
     if (is_empty())
     {
-        throw out_of_range('Stack Underflow!! No more element to pop.')
+        throw out_of_range("Stack Underflow!! No more element to pop.");
     }
 
     // pop element
@@ -108,6 +179,10 @@ void Stack<type, size> :: display()
 template <typename type, int size>
 type Stack<type, size> :: get_top_element()
 {
+    if (is_empty())
+    {
+        throw out_of_range("Stack is empty!!");
+    }
     return stack[top];
 }
 
